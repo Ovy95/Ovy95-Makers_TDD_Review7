207280ac7f1ec0b84081a0e6c1dfd77871afcab2
Ovy95 <jackoverton21@hotmail.com>
2021-04-16 09:33:22 +0100

    returns correct results for single values

--- spec/schoolReportSpec.js
+++ spec/schoolReportSpec.js
@@ -12,8 +12,12 @@ describe("SchoolReport", function () {
       expect(schoolReport.grades("10")).toEqual("Red: 1")
     })
 
-    it("Given a value between 0-49 returns red",function() {
+    it("Given a value between 50 -74 returns Amber",function() {
       expect(schoolReport.grades("52")).toEqual("Amber: 1")
+    })
+
+    it("Given a value between 75-100 returns Green",function() {
+      expect(schoolReport.grades("75")).toEqual("Green: 1")
     })
 
   })
--- src/schoolReport.js
+++ src/schoolReport.js
@@ -10,6 +10,8 @@ class SchoolReport {
 
     }else if(grade > 49 && grade <75){
       return "Amber: 1"
+    }else if(grade > 74 && grade <100){
+      return "Green: 1"
     }
   }
 }
